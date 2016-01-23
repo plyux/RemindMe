85ecd9fe35eb05364425e5797595bc0fa8344e63
Roman <lenin.jun@gmail.com>
2016-01-23 06:06:55 +0200

    Created MainActivity class and layout.

---
+++ RemindMe/app/src/main/java/ua/junior/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package ua.junior.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Вевдюк on 23.01.2016.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
